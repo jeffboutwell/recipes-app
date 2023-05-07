553b007e11b51423d63f13f6e123acdb8b7d6514
Jeff Boutwell <jeffboutwell@gmail.com>
2023-05-07 16:54:30 -0600

    Add social icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom'
-import {Container, Row, Col,Navbar} from 'react-bootstrap'
+import {Container, Row, Col,Navbar,ListGroup} from 'react-bootstrap'
 
 function Footer() {
   return (
@@ -10,7 +10,10 @@ function Footer() {
               <Navbar.Brand href="/">Recipes</Navbar.Brand>
             </Col>
             <Col>
-              <p className="contact"><a href='mailto:jeffboutwell@gmail.com'>jeffboutwell@gmail.com</a></p>
+              <ListGroup horizontal id='social-icons'>
+                <a href='mailto:jeffboutwell@gmail.com' title='Email me'><i className="fa-solid fa-envelope fa-2xl"></i></a>
+                <a href='https://www.instagram.com/jeffb79/' title='Find me on Instagram' target='_blank'><i className="fa-brands fa-square-instagram fa-2xl"></i></a>
+              </ListGroup>
             </Col>
           </Row>
         </Container>
